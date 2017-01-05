c214f3d7a1f283f931fb98271805fe75298861ce
Yanny Castrillon <yannyandres117@gmail.com>
2017-01-05 14:25:31 -0800

    Creation of the video form. Validate admin users to create new videos. Add links in the home page for show and new

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,17 +1,20 @@
 class VideosController < ApplicationController
   # This is like the middleware which gets executed before any action of the controller
-  before_action :authorize_admin, only: [:new,:create,:update,:destroy]
+  before_action :current_user, :authorize_admin, only: [:new,:create,:update,:destroy]
 
+
+  # Home page
   def index
     @videos = Video.all
   end
 
+  # Video description and Information
   def show
-
+    @video = Video.find(params[:id])
   end
 
   def new
-
+    @video = Video.new
   end
 
   def create
