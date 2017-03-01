8d7bd7ba8c2d4ea72078b6a8a6515159bd0e1315
Yanny Castrillon <yannyandres117@gmail.com>
2017-02-28 20:51:19 -0800

    Incorporate transactional code for sensitive code when charging customer amount

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -8,21 +8,24 @@ class Payment < ActiveRecord::Base
   # process payment with Stripe
   def save_with_payment
     if valid?
-      payment_db = Payment.find_by_email(self.email)
-      # validate if payment already has a customer_token
-      if payment_db.stripe_customer_token.present?
-        # Retrieves existing customer Stripe
-        customer = Stripe::Customer.retrieve(self.stripe_customer_token)
-      else
-        # Creates a new Customer on my Stripe account
-        customer = Stripe::Customer.create(description: email,card: stripe_card_token)
-        self.stripe_customer_token = customer.id
-        save!
+      ActiveRecord::Base.transaction do
+        payment_db = Payment.find_by_email(self.email)
+        # validate if payment already has a customer_token
+        if payment_db.stripe_customer_token.present?
+          # Retrieves existing customer Stripe
+          customer = Stripe::Customer.retrieve(self.stripe_customer_token)
+        else
+          # Creates a new Customer on my Stripe account
+          customer = Stripe::Customer.create(description: email,card: stripe_card_token)
+          self.stripe_customer_token = customer.id
+          save!
+        end
+        video_amount = Video.find(self.video_id).price
+        Stripe::Charge.create customer: customer.id,
+                            amount: (video_amount * 100).round,
+                            description: "First Payment",
+                            currency: 'usd'
       end
     end
-    Stripe::Charge.create customer: customer.id,
-                          amount: 3 * 100,
-                          description: "First Payment",
-                          currency: 'usd'
   end
 end
